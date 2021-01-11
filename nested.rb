2e2a008aa48c533fccea5f1eb508794b5f1a18d9
Nathaniel Martinez <nathanielmartinez2001@gmail.com>
2021-01-11 20:01:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,5 +130,8 @@ def adding_to_dennis
       }
     }
     
-  programmer_hash = {:dennis_ritchie => {:languages => ["C", "Assembly"]}
+  programmer_hash = {:dennis_ritchie => {:languages => ["C", "Assembly"]
+    
+     }
+  }
 end
