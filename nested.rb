1076bf2ff6545c023ca971954d197e14b40b85e3
Nathaniel Martinez <nathanielmartinez2001@gmail.com>
2021-01-11 19:33:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,9 +65,7 @@ def adding_matz
 #   :languages => ["LISP", "C"]
 # }
 # return the entire updated hash
-monopoly = {:railroads => {:pieces => 4, :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}, :names => {:reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline_railroad => {}}
-  }
-  
+
 }:languages => ["COBOL", "FORTRAN"]
       },
       :alan_kay => {
