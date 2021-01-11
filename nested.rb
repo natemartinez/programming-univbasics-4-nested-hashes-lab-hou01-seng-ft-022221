06015690f1212d57637fe7f687dd23988adaa9ee
Nathaniel Martinez <nathanielmartinez2001@gmail.com>
2021-01-11 19:31:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,6 +56,7 @@ def dennis_ritchies_language
       }
     }
 programmer_hash[:dennis_ritchie][:languages]
+end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
